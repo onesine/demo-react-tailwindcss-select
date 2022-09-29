918e8b757b28982776f46501f558e457cc826bff
onesine <onesine@dmdconsul.com>
2022-09-29 17:33:07 +0100

    Update react-select-package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}", "./node_modules/react-tailwindcss-select/dist/index.esm.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [/*require('@tailwindcss/forms')*/],
+    content: [
+        "./public/index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./node_modules/react-tailwindcss-select/dist/index.esm.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
 }
